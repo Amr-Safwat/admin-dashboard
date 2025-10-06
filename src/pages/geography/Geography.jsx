775762994a230054f223f4e16dfba5cf0a048f92
Amr-Safwat <amrsafwat02@gmail.com>
2025-10-06 11:36:15 +0300

    Separate the shape in a separate component

--- src/pages/geography/Geography.jsx
+++ src/pages/geography/Geography.jsx
@@ -1,45 +1,9 @@
-import {ResponsiveChoropleth} from '@nivo/geo';
-import { data } from './data';
-import { Box } from '@mui/material';
-import {geography} from './world_countries';  
+import {MapChart} from '../../components/Charts/Map Chart/MapChart';
 
-
-export const Geography = () => { 
+export const Geography = () => {
   return (
-    <Box sx={{height: '75vh', border: '1px solid white'}}>
-      <ResponsiveChoropleth /* or Choropleth for fixed dimensions */
-        projectionScale={150}
-        data={data}
-        features={geography.features}
-        margin={{top: 0, right: 0, bottom: 0, left: 0}}
-        colors="nivo"
-        domain={[0, 1000000]}
-        unknownColor="#666666"
-        label="properties.name"
-        valueFormat=".2s"
-        enableGraticule={true}
-        // graticuleLineColor="#dddddd"
-        borderWidth={0.5}
-        borderColor="#152538"
-        legends={[
-          {
-            anchor: 'bottom-left',
-            direction: 'column',
-            justify: true,
-            translateX: 20,
-            translateY: -100,
-            itemsSpacing: 0,
-            itemWidth: 94,
-            itemHeight: 18,
-            itemDirection: 'left-to-right',
-            itemTextColor: '#444444',
-            itemOpacity: 0.85,
-            symbolSize: 18,
-          },
-        ]}
-      />
-    </Box>
+    <>
+      <MapChart />
+    </>
   );
-}
-  
-
+};
